c225933e76be8677881edb71a203910e32edbd66
Ludwig <00ludwighahn@gmail.com>
2024-09-26 23:16:52 +0200

    Dynamic filepath

--- RealEstate.Core/Services/EstateDataService.cs
+++ RealEstate.Core/Services/EstateDataService.cs
@@ -15,8 +15,19 @@ namespace RealEstate.Core.Services
     public class EstateDataService : IEstateDataService
     {
         // private const string FilePath = "C:\\Users\\ludwi\\source\\repos\\RealEstate.Core\\Services\\estates.json.txt.json"; // Path to the JSON file
-        private const string FilePath = "C:\\Users\\ludwi\\source\\repos\\RealEstate-Solution\\RealEstate.Core\\Services\\estates.json";
+        //private const string FilePath = "C:\\Users\\ludwi\\source\\repos\\RealEstate-Solution\\RealEstate.Core\\Services\\estates.json";
+        private readonly string FilePath;
 
+
+        public EstateDataService()
+        {
+            // Navigate from bin\Debug to RealEstate.Core\Services
+            var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            FilePath = Path.Combine(baseDirectory, "..", "..", "..", "..", "RealEstate.Core", "Services", "estates.json");
+
+            // Normalize the path
+            FilePath = Path.GetFullPath(FilePath);
+        }
         // Return the list of estates, either from JSON or with mock data
         public async Task<IEnumerable<Estate>> GetEstatesAsync()
         {

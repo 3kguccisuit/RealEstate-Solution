245f3649e326f9069d9c73de4b9f9bb1ee934aee
Ludwig <00ludwighahn@gmail.com>
2024-10-15 03:12:48 +0200

    SaveAsJson

--- RealEstateBLL/Service/DataService.cs
+++ RealEstateBLL/Service/DataService.cs
@@ -1,21 +1,21 @@
 ﻿using DTO.Models;
-//using RealEstate.Models;
+using RealEstateDAL.Files;
 
 namespace RealEstateBLL.Service
 {
     public class DataService
     {
         //private readonly AppState _appState;
-
+        private readonly FileRepository _fileRepository;
         public DataService()
         {
-            // Load data from the database
+            _fileRepository = new FileRepository();
         }
 
         public void SaveDataAsJson(RootObject lists, string filePath)
         {
             Console.WriteLine("Saving data as JSON");
-
+            _fileRepository.SaveDataAsJson(lists, filePath);
             // Save data to the database
         }
     }

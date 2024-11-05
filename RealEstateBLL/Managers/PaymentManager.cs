556c2a8d4fb526b3912052e38042051e4ff3044a
Ludwig <00ludwighahn@gmail.com>
2024-11-05 13:28:22 +0100

    Added GetPersonsByCity and GetPaymentsByType

--- RealEstateBLL/Managers/PaymentManager.cs
+++ RealEstateBLL/Managers/PaymentManager.cs
@@ -5,5 +5,11 @@ namespace RealEstateDLL.Managers
     public class PaymentManager : DictionaryManager<string, Payment>
     {
         // Inherits functionality from DictionaryManager<string, Payment>
+        public List<Payment> GetPaymentsByType(string type)
+        {
+            return GetAll()
+                .Where(payment => payment.Type.Equals(type, StringComparison.OrdinalIgnoreCase))
+                .ToList();
+        }
     }
 }
